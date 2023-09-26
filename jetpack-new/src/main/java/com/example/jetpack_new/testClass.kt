23393e40085dcaaf13b219380dcbb169cbb54fa1
Andrey Pogodin <andreyweather@gmail.com>
2023-09-26 21:40:50 +0300

    Initial commit

---
+++ jetpack-new/src/main/java/com/example/jetpack_new/testClass.kt
@@ -0,0 +1,10 @@
+package com.example.jetpack_new
+
+import android.content.Context
+import android.widget.Toast
+
+class testClass {
+    fun go (context: Context) {
+        Toast.makeText(context, "go", Toast.LENGTH_SHORT).show()
+    }
+}
