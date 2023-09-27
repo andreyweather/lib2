539b6b087e8d7ec202c09b1001af39d419a90563
Andrey Pogodin <andreyweather@gmail.com>
2023-09-27 15:21:11 +0300

    commit

--- jetpack-new/build.gradle.kts
+++ jetpack-new/build.gradle.kts
@@ -36,12 +36,30 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    implementation ("com.google.android.material:material:1.5.0-alpha02")
+    implementation ("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation ("com.google.firebase:firebase-messaging-ktx:23.2.1")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation ("com.google.code.gson:gson:2.8.9")
+
+    implementation("androidx.core:core-ktx:1.9.0")
+
+    implementation("com.google.android.material:material:1.9.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
 publishing {
     publications {
