0723546385b1f83787136a2ded1783104499592a
Andrey Pogodin <andreyweather@gmail.com>
2023-09-27 15:59:55 +0300

    commit

--- app/src/main/java/com/example/jetpack_new/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_new/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.jetpack_new
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enkod.enkodpushlibrary.enkodConnect
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //enkodConnect("andrey_p_client","test_newlibrary@enkod.io").start(this)
     }
 }
