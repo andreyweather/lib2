98beeab168f18627dbb697637b920784e6d4ae25
Andrey Pogodin <andrey.pogodin@enkod.io>
2023-12-13 15:55:40 +0300

    commit

--- epl/build.gradle.kts
+++ epl/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.jetpack_new"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 24
@@ -38,20 +38,20 @@ android {
 
 dependencies {
     implementation("com.google.android.material:material:1.9.0")
+    implementation ("com.google.firebase:firebase-messaging:23.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
-    implementation("com.google.android.material:material:1.5.0-alpha02")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("com.google.firebase:firebase-messaging-ktx:23.2.1")
-    implementation("com.squareup.picasso:picasso:2.71828")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.8.0")
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    implementation ("com.google.android.material:material:1.5.0-alpha02")
+    implementation ("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-    implementation("com.google.code.gson:gson:2.8.9")
+    implementation ("com.google.code.gson:gson:2.8.9")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
