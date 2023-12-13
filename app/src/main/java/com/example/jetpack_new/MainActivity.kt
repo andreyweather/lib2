8a94773186a38f40964f680b892de573fd519d2e
Andrey Pogodin <andrey.pogodin@enkod.io>
2023-12-13 15:25:49 +0300

    commit

--- app/src/main/java/com/example/jetpack_new/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_new/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.jetpack_new
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.enkod.enkodpushlibrary.EnkodPushLibrary
 import com.enkod.enkodpushlibrary.enkodConnect
 import com.example.jetpack_new.databinding.ActivityMainBinding
@@ -9,12 +11,13 @@ import com.example.jetpack_new.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        enkodConnect("andrey_pogodin2", "newlibrarytest7@gmail.com", "89066495000").start(this)
+        enkodConnect("andrey_pogodin3").start(this)
         binding.addCont.setOnClickListener {
             EnkodPushLibrary.addContact("newlibrarytest7@gmail.com")
             binding.contactIndecator.text = "contact on"
