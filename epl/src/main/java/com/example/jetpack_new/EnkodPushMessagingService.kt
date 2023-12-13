b1c1b5912b4b9787bd79df1877f304233bc5fde5
Andrey Pogodin <andrey.pogodin@enkod.io>
2023-12-13 16:49:21 +0300

    Commit

--- epl/src/main/java/com/example/jetpack_new/EnkodPushMessagingService.kt
+++ epl/src/main/java/com/example/jetpack_new/EnkodPushMessagingService.kt
@@ -1,33 +1,48 @@
 package com.enkod.enkodpushlibrary
 
 import android.app.Activity
+import android.app.ActivityManager
 import android.content.ContentValues
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
 import android.util.Log
-import com.enkod.enkodpushlibrary.EnkodPushLibrary.account
+import androidx.annotation.RequiresApi
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.LazyHeaders
+import com.enkod.enkodpushlibrary.EnkodPushLibrary.initLateInit
 import com.enkod.enkodpushlibrary.EnkodPushLibrary.initRetrofit
 import com.enkod.enkodpushlibrary.EnkodPushLibrary.processMessage
-import com.enkod.enkodpushlibrary.EnkodPushLibrary.sessionId
-import com.enkod.enkodpushlibrary.EnkodPushLibrary.token
-import com.example.jetpack_new.R
+import com.example.enkodpushlibrary.MyService
+import com.example.jetpack_new_1.R
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import rx.Observable
+import rx.Observer
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import java.util.concurrent.Callable
 
-private  val TAGL = "LoginData"
-private  val ACC_TAG: String = "${TAGL}_ACC"
+
+private val TAGL = "LoginData"
+private val ACC_TAG: String = "${TAGL}_ACC"
 
 
+class EnkodPushMessagingService : FirebaseMessagingService() {
 
-
-class EnkodPushMessagingService: FirebaseMessagingService() {
+    private val TAG = "EnkodPushLibrary"
+    private val EXIT_TAG: String = "${TAG}_EXIT"
 
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
 
-          Log.d("onNewToken", token.toString())
+        Log.d("onNewToken", token.toString())
+
     }
 
     override fun onDeletedMessages() {
@@ -36,13 +51,22 @@ class EnkodPushMessagingService: FirebaseMessagingService() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onMessageReceived(message: RemoteMessage) {
+
+
+        Log.d("onMessageReceived", "connect")
+
         super.onMessageReceived(message)
 
+        // EXIT_TAG - preferences с содержанием поля "noexit" открывают доступ к методу startForeground() в Myservice
 
-        val directBootContext: Context = this.createDeviceProtectedStorageContext()
-// Access appDataFilename that lives in device encrypted storage
-        Log.d ("message_app_test", message.data.toString())
+        val preferences = applicationContext.getSharedPreferences(TAG, Context.MODE_PRIVATE)
+        preferences.edit()
+
+            .putString(EXIT_TAG, "noexit")
+            .apply()
+
 
         EnkodPushLibrary.soundOn = applicationContext.getString(R.string.sound_on)
         EnkodPushLibrary.vibrationOn = applicationContext.getString(R.string.vibration_on)
@@ -52,7 +76,8 @@ class EnkodPushMessagingService: FirebaseMessagingService() {
         EnkodPushLibrary.ledOffMs = applicationContext.getString(R.string.led_of_ms)
         EnkodPushLibrary.colorName = applicationContext.getString(R.string.color_name)
         EnkodPushLibrary.iconRes = applicationContext.getString(R.string.icon_res)
-        EnkodPushLibrary.notificationPriority = applicationContext.getString(R.string.notification_priority)
+        EnkodPushLibrary.notificationPriority =
+            applicationContext.getString(R.string.notification_priority)
         EnkodPushLibrary.bannerName = applicationContext.getString(R.string.banner_name)
         EnkodPushLibrary.intentName = applicationContext.getString(R.string.intent_name)
         EnkodPushLibrary.channelId = applicationContext.getString(R.string.channel_id)
@@ -68,38 +93,112 @@ class EnkodPushMessagingService: FirebaseMessagingService() {
         EnkodPushLibrary.personId = applicationContext.getString(R.string.person_id)
         EnkodPushLibrary.messageId = applicationContext.getString(R.string.message_id)
 
+
+
+        fun isAppInforegrounded(): Boolean {
+            val appProcessInfo = ActivityManager.RunningAppProcessInfo();
+            ActivityManager.getMyMemoryState(appProcessInfo);
+            return (appProcessInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND ||
+                    appProcessInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_VISIBLE)
+        }
+
+
+        if (!isAppInforegrounded()) {
+
+            val service = Intent(this, MyService::class.java)
+
+            this.startForegroundService(service)
+
+            imageCheckAndStartLoad(message)
+
+        }
+
+        else {
+
+            imageCheckAndStartLoad(message)
+
+        }
+
         initRetrofit()
+        initLateInit(this)
 
-        processMessage(applicationContext, message)
+    }
 
 
+    fun imageCheckAndStartLoad(message: RemoteMessage) {
+        if (message.data["image"].isNullOrEmpty()) {
 
+            processMessage(this, message, null)
+
+        } else {
+            downloadImageToPush(this, message)
+        }
+    }
+
+
+    fun downloadImageToPush(context: Context, message: RemoteMessage) {
+
+        val userAgent = System.getProperty("http.agent")
+
+        val url = GlideUrl(
+
+            message.data["image"], LazyHeaders.Builder()
+                .addHeader(
+                    "User-Agent",
+                    userAgent
+                )
+                .build()
+        )
+
+        Observable.fromCallable(object : Callable<Bitmap?> {
+            override fun call(): Bitmap? {
+                val future = Glide.with(context).asBitmap()
+                    .timeout(30000)
+                    .load(url).submit()
+                return future.get()
+            }
+        }).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : Observer<Bitmap?> {
+
+                override fun onCompleted() {
+
+                }
+
+                override fun onError(e: Throwable) {
+
+                    Log.d("onError", e.message.toString())
+                    processMessage(context, message, null)
+
+                }
+
+                override fun onNext(t: Bitmap?) {
+                    processMessage(context, message, t!!)
+                    Log.d("onNext", t.toString())
+                    EnkodPushLibrary.exitSelf()
+
+                }
+            })
     }
 }
 
 
+class enkodConnect(_account: String) : Activity() {
 
-class enkodConnect (_account: String, _email: String, _phone: String) : Activity() {
 
     private val TAG = "EnkodPushLibrary"
-    private val SESSION_ID_TAG: String = "${TAG}_SESSION_ID"
-    private val EMAIL_TAG: String = "${TAG}_EMAIL"
-    private val PHONE_TAG: String = "${TAG}_PHONE"
+
 
     val account: String
-    val email: String
-    val phone: String
-
-
 
     init {
         account = _account
-        email = _email
-        phone = _phone
     }
 
 
-    fun start (context: Context) {
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun start(context: Context) {
+
 
         Log.d("start", "ok")
 
@@ -117,20 +216,16 @@ class enkodConnect (_account: String, _email: String, _phone: String) : Activity
 
                 val token = task.result
                 EnkodPushLibrary.getToken(context, token)
+
             })
 
-            EnkodPushLibrary.init(context, account, email, phone, 1)
+            EnkodPushLibrary.init(context, account, 1)
 
         } catch (e: Exception) {
 
-            EnkodPushLibrary.init(context, account, email, phone, 0)
+            EnkodPushLibrary.init(context, account, 0)
 
         }
     }
 }
 
-private class ForcedException : Exception() {
-    fun forcedThrow() {
-        throw ForcedException()
-    }
-}
