25cc1f7dc994fd841d84934c6f92e04438a2886a
Andrey Pogodin <andrey.pogodin@enkod.io>
2023-12-13 16:38:25 +0300

    Commit

---
+++ app/src/main/java/com/example/jetpack_new/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.jetpack_new
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.enkod.enkodpushlibrary.EnkodPushLibrary
+import com.enkod.enkodpushlibrary.enkodConnect
+import com.example.jetpack_new.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enkodConnect("andrey_pogodin2", "newlibrarytest7@gmail.com", "89066495000").start(this)
+        binding.addCont.setOnClickListener {
+            EnkodPushLibrary.addContact("newlibrarytest7@gmail.com")
+            binding.contactIndecator.text = "contact on"
+        }
+
+    }
+}
